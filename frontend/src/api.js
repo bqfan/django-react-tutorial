fb78b9be83b67e2e2fd1afc2a02627c62450ba74
Baiquan Fan <baiquan.fan@gmail.com>
2024-04-26 20:46:42 +0300

    add backedn URL.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+const apiUrl = "https://a70c35fb-90e8-4d6a-8246-c125949efd16-dev.e1-eu-north-azure.choreoapis.dev/djangoreacttutorial/backend/rest-api-be2/v1.0"
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl
 });
 
 api.interceptors.request.use(
